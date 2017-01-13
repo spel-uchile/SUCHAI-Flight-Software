0d6d62bfd02ece72fb87a9657f1321e8cbad7eae
iibanez <ignacio.ibanez@usach.cl>
2017-01-13 15:24:14 -0300

    List of test commands for different types of parameters and format

---
+++ System/cmdTestCommand.c
@@ -0,0 +1,52 @@
+/**
+ * @file  cmdTestCommand.h
+ * @author Ignacio Ibañez A - ignacio.ibanez@usach.cl
+ * @date 2016
+ * @copyright GNU GPL v3
+ *
+ * This header have definitions related to command repository
+ */
+
+#include "cmdTestCommand.h"
+
+void test_cmd_init(void)
+{
+
+    cmd_add("cmd1", test_cmd_str_int, "%s %i", 2);
+    cmd_add("cmd2", test_cmd_double_int, "%f %f %i %i", 4);
+    cmd_add("cmd3", test_cmd_str_double_int, "%s %f %s %f %i", 5);
+
+}
+
+int test_cmd_str_int(char *fmt, char *params, int nparams)
+{
+    char msg[10];
+    int valor = 0;
+
+    assertf(sscanf(params,fmt, msg, &valor) == nparams, "\n[Debug Error] The format of parameters are: %s and parameters used are: %s\n",fmt, params);
+    printf("[Debug Msg] %s: %s_%i\n","con_str_int" ,msg,valor);
+    return CMD_OK;
+}
+
+int test_cmd_double_int(char *fmt, char *params, int nparams)
+{
+    float v1 = 0, v2 = 0;
+    int v3 = 0,v4 = 0;
+
+    assertf(sscanf(params,fmt, &v1, &v2, &v3, &v4) == nparams,  "\n[Debug Error] The format of parameters are: %s and parameters used are: %s\n",fmt, params);
+    printf("[Debug Msg] %s: %f_%f_%i_%i\n", "con_double_int",v1,v2,v3,v4);
+    return CMD_OK;
+}
+
+int test_cmd_str_double_int(char *fmt, char *params, int nparams)
+{
+    char v1[10];
+    char v3[10];
+    float v2 = 0, v4 = 0;
+    int v5 = 0;
+
+    assertf( sscanf(params,fmt, v1, &v2, v3, &v4, &v5) == nparams, "\n[Debug Error] The format of parameters are: %s and parameters used are: %s\n",fmt, params);
+    printf("[Debug Msg] %s: %s_%f_%s_%f_%i\n","str_double_int",v1,v2,v3,v4,v5);
+    return CMD_OK;
+
+}
---
+++ System/include/cmdTestCommand.h
@@ -0,0 +1,65 @@
+/**
+ * @file  cmdTestCommand.h
+ * @author Ignacio Ibañez A - ignacio.ibanez@usach.cl
+ * @date 2016
+ * @copyright GNU GPL v3
+ *
+ * This header have definitions related to command repository
+ */
+
+#ifndef CMD_TEST_COMMAND_H
+#define CMD_TEST_COMMAND_H
+
+#include "SUCHAI_config.h"
+#include "repoCommand.h"
+#include <errno.h>
+#include <assert.h>
+
+#define clean_errno() (errno == 0 ? "None" : strerror(errno))
+#define log_error(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define assertf(A, M, ...) if(!(A)) {log_error(M, ##__VA_ARGS__); assert(A); }
+
+/* Function definitions */
+/**
+ * Initializes command repository the cmdTestCommand.h
+ *
+ * @param None
+ * @return None
+ */
+void test_cmd_init(void);
+
+/**
+ * Test commands with string and integers
+ *
+ * @param fparams Pointer to parameter format
+ * @param params Pointer to parameters
+ * @param nparam Numbers of parameters
+ * @return  One if the command was executed correctly
+ *          Zero if the command failed
+ */
+int test_cmd_str_int(char *fmt, char *params, int nparams);
+
+/**
+ * Test commands with double and integers
+ *
+ * @param fparams Pointer to parameter format
+ * @param params Pointer to parameters
+ * @param nparam Numbers of parameters
+ * @return  One if the command was executed correctly
+ *          Zero if the command failed
+ */
+int test_cmd_double_int(char *fmt, char *params, int nparams);
+
+/**
+ * Test commands with string, double and integers
+ *
+ * @param fparams Pointer to parameter format
+ * @param params Pointer to parameters
+ * @param nparam Numbers of parameters
+ * @return  One if the command was executed correctly
+ *          Zero if the command failed
+ */
+int test_cmd_str_double_int(char *fmt, char *params, int nparams);
+
+
+#endif //CMD_TEST_COMMAND_H
