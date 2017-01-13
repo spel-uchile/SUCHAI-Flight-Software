b002e3e75efae5e869418081db5b28a6b5afa2c0
Carlos Gonzalez C <carlgonz@ug.uchile.cl>
2017-01-13 16:49:57 -0300

    Fixing warning in va_start

--- System/repoCommand.c
+++ System/repoCommand.c
@@ -108,7 +108,7 @@ void cmd_add_params_str(cmd_t *cmd, char *params)
 void cmd_add_params_var(cmd_t *cmd, ...)
 {
     va_list args;
-    va_start(args, cmd->fmt);
+    va_start(args, cmd);
 
     //Parsing arguments to string
     char str_params[CMD_MAX_STR_PARAMS];
