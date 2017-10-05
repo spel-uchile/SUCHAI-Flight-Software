45803addcadc62cc4678c34ac5eda2ae4e0037c7
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2017-10-05 19:48:11 -0300

    Adding script to build libcsp

---
+++ drivers/Linux/libcsp/install_csp.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "Downloading libcsp"
+[ ! -e libcsp ] && git clone git@github.com:libcsp/libcsp.git
+cd libcsp
+
+echo "Build libcsp"
+./waf configure --with-os=posix --enable-if-kiss --enable-if-zmqhub --install-csp --prefix=../ build install
+
+cd -
