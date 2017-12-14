83763886fac9902f0778c1337a260ee1ce89e505
Grynn01 <matias.ramirez@ing.uchile.cl>
2017-12-14 15:40:19 -0300

    Starting the taskFlightPlan code

--- src/system/taskFlightPlan.c
+++ src/system/taskFlightPlan.c
@@ -5,6 +5,8 @@
 #include "taskFlightPlan.h"
 
 static const char *tag = "FlightPlan";
+char* table = "flight-plan";
+int timeinit=0;   // ################################################################  Must change  ###################
 
 void taskFlighPlan (void *param){
 
@@ -13,20 +15,31 @@ void taskFlighPlan (void *param){
     portTick delay_ms = 10000;          //Task period in [ms]
 
     unsigned int elapsed_sec = 0;      // Seconds counter
-    //unsigned int sec_check = 10;     // seconds to check actions
 
     char *task_name = malloc(sizeof(char)*14);
     strcpy(task_name, "FlightPlan");
 
     portTick xLastWakeTime = osTaskGetTickCount();
 
+    storage_table_flight_plan_init_times(timeinit,table);
+
+    const unsigned char* command;
+    const unsigned char* args;
+    int repeat;
+
     while(1){
 
         osTaskDelayUntil(&xLastWakeTime, delay_ms); //Suspend task
         elapsed_sec += delay_ms; //Update seconds counts
 
+        command = storage_flight_plan_get_command(elapsed_sec,table);
+        args = storage_flight_plan_get_args(elapsed_sec,table);
+        repeat = storage_flight_plan_get_repeat(elapsed_sec,table);
 
-
+        for(int i=0; i<repeat; i++)
+        {
+            //ejecutar el commando     
+        }
 
     }
 }
