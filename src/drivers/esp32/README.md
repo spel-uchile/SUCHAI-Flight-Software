13abff3e25196772e955209eff5a1dd6131e40bd
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2017-12-28 17:54:44 -0300

    Adding ESP32 drivers

---
+++ src/drivers/esp32/README.md
@@ -0,0 +1,5 @@
+# ESP32 Drivers
+
+1. To setup the esp32 development environment go to http://esp-idf.readthedocs.io/en/latest/linux-setup.html
+2. Execute ./install.sh to dowload toolchain and drivers
+3. Setup environment variables in ~/.basrch file
---
+++ src/drivers/esp32/install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "Downloading toolchain..."
+wget https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-61-gab8375a-5.2.0.tar.gz
+tar -xzf xtensa-esp32-elf-linux64-1.22.0-61-gab8375a-5.2.0.tar.gz
+
+echo "Downloading ESP-IDF...."
+git clone --recursive https://github.com/espressif/esp-idf.git
+cd esp-idf
+git submodule update --init
+cd ..
+
+echo "Fixing FreeRTOS includes..."
+sed -i "s/COMPONENT_ADD_INCLUDEDIRS := include/COMPONENT_ADD_INCLUDEDIRS := include include\/freertos/g" esp-idf/components/freertos/component.mk
+
+echo "---------"
+echo "Finished"
+echo "Add the following lines to your bash.rc file"
+echo "export PATH=\$PATH:$(pwd)/xtensa-esp32-elf/bin"
+echo "export IDF_PATH=$(pwd)/esp-idf"
+echo "---------"
