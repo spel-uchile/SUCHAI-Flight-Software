507e922ba727e7bbd288677787bf4ae9479371dd
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2018-04-10 16:01:37 -0300

    Adding commands to reset wdt, reset the obc and debug the obc

--- src/system/cmdOBC.c
+++ src/system/cmdOBC.c
@@ -1,9 +1,9 @@
 /*                                 SUCHAI
  *                      NANOSATELLITE FLIGHT SOFTWARE
  *
- *      Copyright 2017, Carlos Gonzalez Cortes, carlgonz@ug.uchile.cl
- *      Copyright 2017, Tomas Opazo Toro, tomas.opazo.t@gmail.com
- *      Copyright 2017, Matias Ramirez Martinez, nicoram.mt@gmail.com
+ *      Copyright 2018, Carlos Gonzalez Cortes, carlgonz@ug.uchile.cl
+ *      Copyright 2018, Tomas Opazo Toro, tomas.opazo.t@gmail.com
+ *      Copyright 2018, Matias Ramirez Martinez, nicoram.mt@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,11 +25,58 @@ static const char* tag = "cmdOBC";
 
 void cmd_obc_init(void)
 {
+    cmd_add("debug_obc", obc_debug, "%d", 1);
     cmd_add("reset", obc_reset, "", 0);
     cmd_add("get_mem", obc_get_os_memory, "", 0);
     cmd_add("set_time", obc_set_time,"%d",1);
     cmd_add("show_time", obc_show_time,"%d",1);
     cmd_add("test_fp", test_fp, "%d %s %d", 3);
+    cmd_add("reset_wdt", obc_reset_wdt, "", 0);
+}
+
+int obc_debug(char *fmt, char *params, int nparams)
+{
+    int dbg_type;
+    if(sscanf(params, fmt, &dbg_type) == nparams)
+    {
+        #ifdef AVR32
+            switch(dbg_type)
+            {
+                case 0: LED_Toggle(LED0); break;
+                case 1: LED_Toggle(LED1); break;
+                case 2: LED_Toggle(LED2); break;
+                case 3: LED_Toggle(LED3); break;
+                default: LED_Toggle(LED0);
+            }
+        #endif
+        #ifdef NANOMIND
+            if(dbg_type <= LED_A)
+                led_toggle((led_names_t)dbg_type);
+        #endif
+        #ifdef ESP32
+            statinc int level = 0;
+            level = ~level;
+            gpio_set_level(BLINK_GPIO, level);
+        #endif
+        #ifdef LINUX
+            LOGD(tag, "OBC Debug (%d)", dbg_type);
+        #endif
+        return CMD_OK;
+    }
+    LOGW(tag, "Command obc_debug used with invalid param!");
+    return CMD_FAIL;
+}
+
+int obc_reset_wdt(char *fmt, char *params, int nparams)
+{
+    int rc = CMD_OK;
+    #ifdef NANOMIND
+       wdt_clear();
+    #endif
+    #ifdef AVR32
+       wdt_clear();
+    #endif
+    return rc;
 }
 
 int obc_reset(char *fmt, char *params, int nparams)
@@ -38,8 +85,12 @@ int obc_reset(char *fmt, char *params, int nparams)
 
     #ifdef LINUX
         raise(SIGINT);
-    #else
-        //TODO __asm__ volatile("reset");
+    #endif
+    #ifdef AVR32
+        reset_do_soft_reset();
+    #endif
+    #ifdef NANOMIND
+        cpu_reset();
     #endif
 
     /* Never get here */
@@ -111,6 +162,7 @@ int obc_show_time(char* fmt, char* params,int nparams)
     }
 }
 
+// #FIXME: This function do not belong here (carlgonz)
 int test_fp(char* fmt, char* params,int nparams)
 {
     int num1, num2;
--- src/system/include/cmdOBC.h
+++ src/system/include/cmdOBC.h
@@ -2,7 +2,7 @@
  * @file  cmdPPC.h
  * @author Carlos Gonzalez C - carlgonz@uchile.cl
  * @author Tomas Opazo T - tomas.opazo.t@gmail.com
- * @date 2017
+ * @date 2018
  * @copyright GNU GPL v3
  *
  * This header have definitions of commands related to the on board computer
@@ -13,6 +13,25 @@
 #define CMD_OBC_H
 
 #include <signal.h>
+#ifdef LINUX
+#endif
+
+#ifdef AVR32
+    #include "led.h"
+    #include "wdt.h"
+    #include "avr32_reset_cause.h"
+#endif
+
+#ifdef NANOMIND
+    #include "led.h"
+    #include "wdt.h"
+    #include "dev/cpu.h"
+#endif
+
+#ifdef ESP32
+    #define BLINK_GPIO CONFIG_BLINK_GPIO  // Set in menuconfig
+    #include "driver/gpio.h"
+#endif
 
 #include "config.h"
 #include "repoCommand.h"
@@ -22,6 +41,28 @@
  * Register on board computer related (OBC) commands
  */
 void cmd_obc_init(void);
+
+/**
+ * Check if the OBC is working, usually blinking a led or printing a debug
+ * message. In AVR32, NANOMIND and ESP32 blink the led indicated in the
+ * parameter in LINUX just print a debug message.
+ *
+ * @param fmt Str. Parameters format "d"
+ * @param params Str. Parameters as string "1"
+ * @param nparams Int. Number of parameters 1
+ * @return  CMD_OK if executed correctly or CMD_FAIL in case of errors
+ */
+int obc_debug(char *fmt, char *params, int nparams);
+
+/**
+ * Reset the watchdog timer
+ *
+ * @param fmt Str. Parameters format ""
+ * @param params Str. Parameters as string ""
+ * @param nparams Int. Number of parameters 0
+ * @return  CMD_OK if executed correctly or CMD_FAIL in case of errors
+ */
+int obc_reset_wdt(char *fmt, char *params, int nparams);
 
 /**
  * Reset the system
