07d1e621781c7232d7d4c9ac21626d7d1fefe93c
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2018-05-28 17:20:39 -0400

    Add CSP support to zmqhub and zmqnode python scripts. This helps to monitor and debug CSP communications over ZMQ.

--- sandbox/zmqhub.py
+++ sandbox/zmqhub.py
@@ -2,20 +2,29 @@ import zmq
 import argparse
 from threading import Thread
 
+prompt = "<origin> <dest> <port> <message>: "
+
+
 def parse_csp(hdrhex):
+    """
+    Parse and print CSP header
+    :param hdrhex: Str. Header as hexadecimal in a string
+    :return: Str. Header parsed as string
+    """
     hdrhex = int(hdrhex, 16)
     header = "S {}, D {}, Dp {}, Sp {}, Pr {}, HMAC {} XTEA {} RDP {} CRC32 {}".format(
-        (hdrhex >> 30) & 0x03,
         (hdrhex >> 25) & 0x1f,
         (hdrhex >> 20) & 0x1f,
         (hdrhex >> 14) & 0x3f,
         (hdrhex >> 8) & 0x3f,
+        (hdrhex >> 30) & 0x03,
         "Yes" if ((hdrhex >> 3) & 0x01) else "No",
         "Yes" if ((hdrhex >> 2) & 0x01) else "No",
         "Yes" if ((hdrhex >> 1) & 0x01) else "No",
         "Yes" if ((hdrhex >> 0) & 0x01) else "No")
 
     return header
+
 
 def monitor(port="8003"):
     """ Monitor socket running in background """
@@ -27,40 +36,55 @@ def monitor(port="8003"):
     while True:
         frame = sock.recv_multipart()[0]
         # header = ["{:08b}".format(i) for i in frame[1:5]]
-        # print(header)
-        header = ["{:08b}".format(i) for i in frame[1:5]][::-1]
-        print(header)
+        # print("\nmon:", header)
+        # header = ["{:08b}".format(i) for i in frame[1:5]][::-1]
+        # print("mon:", header)
         header_a = ["{:02x}".format(i) for i in frame[1:5]]
-        # print(header)
+        # print("mon:", header_a)
         header = "0x"+"".join(header_a[::-1])
+        # print("mon:", header)
         data = frame[5:]
         try:
             csp_header = parse_csp(header)
         except:
             csp_header = ""
-        print('MON:', frame)
-        print('Header: {},'.format(csp_header), "Data: {}".format(data))
+
+        # Print the header and message
+        print('\nmon:', frame)
+        print('\tHeader: {},'.format(csp_header))
+        print("\tData: {}".format(data))
+        print(prompt)
 
 def console(port="8001"):
+    """ Console socket to send CSP messages """
     import re
     ctx = zmq.Context(1)
     sock = ctx.socket(zmq.PUB)
     sock.connect('tcp://localhost:{}'.format(port))
-    #          Prio  SRC   DST     DP     SP   RES HXRC
+
+    #          Prio   SRC   DST    DP   SP  RES HXRC
     header = "{:02b}{:05b}{:05b}{:06b}{:06b}00000000"
+
     while True:
-        cmd = input("console >> ").split(" ", 1)
-        dst, data = int(cmd[0]), cmd[1]
-        hdr = header.format(1, 11, dst, 1, 1)
-        #print(hdr, data)
-        print(parse_csp(hex(int(hdr, 2))), data)
-        hdr_b = re.findall("........",hdr)[::-1]
-        print(hdr_b)
-        hdr = "".join([chr(int(i,2)) for i in hdr_b])
-        msg = chr(dst) + hdr + (data)
-        msg = msg.encode("ascii", "replace")
-        print(msg)
-        sock.send(msg)
+        try:
+            # Get CSP header and data
+            cmd = input(prompt).split(" ")
+            node, dst, port, data = int(cmd[0]), int(cmd[1]), int(cmd[2]), cmd[3]
+            # print("con:", "{:05b}".format(node), "{:05b}".format(dst), "{:06b}".format(port), data)
+            hdr = header.format(1, node, dst, port, 33)
+            # print("con:", hdr, hex(int(hdr, 2)))
+            # print(hdr, data)
+            # print("con:", parse_csp(hex(int(hdr, 2))), data)
+
+            # Build CSP message
+            hdr_b = re.findall("........",hdr)[::-1]
+            # print("con:", hdr_b, ["{:02x}".format(int(i, 2)) for i in hdr_b])
+            hdr = bytearray([int(i,2) for i in hdr_b])
+            msg = bytearray(dst) + hdr + bytearray(data, "ascii")
+            # print("con:", msg)
+            sock.send(msg)
+        except Exception as e:
+            print(e)
 
 def get_parameters():
     """ Parse command line parameters """
@@ -92,12 +116,14 @@ if __name__ == "__main__":
         s_mon.bind('tcp://*:{}'.format(args.mon_port))
         # Start monitor thread
         mon_th = Thread(target=monitor, args=(args.mon_port,))
+        mon_th.daemon = True
         mon_th.start()
 
-    # if args.con:
-    #     # Create a console socket
-    #     con_th = Thread(target=console, args=(args.in_port, ))
-    #     con_th.start()
+    if args.con:
+        # Create a console socket
+        con_th = Thread(target=console, args=(args.in_port, ))
+        con_th.daemon = True
+        con_th.start()
 
     # Start ZMQ proxy (blocking)
     zmq.proxy(xpub_out, xsub_in, s_mon)
