b9b558a4d54ce388244da02bb74fa9412ac91108
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2018-07-25 18:21:53 -0400

    Improves osTaskDelayUntil test and function. Now the osTaskDelayUntil can recover the lost time.

--- test/test_bug_delay/src/system/taskTest.c
+++ test/test_bug_delay/src/system/taskTest.c
@@ -1,6 +1,5 @@
 #include "include/taskTest.h"
 
-#define TEST_FAILS 1
 #define TEST_MAX_SECONDS 5
 
 const static char *tag = "taskTest";
@@ -10,20 +9,40 @@ void taskTest(void *param)
     char *tag = (char *)param;
     LOGI(tag, "Started");
 
-    int i = 0;
+    int i, work = 0;
 
+    srand(time(NULL));
     portTick xStartTime = osTaskGetTickCount();
     portTick xLastTime = xStartTime;
 
     for(i=0; i<TEST_MAX_SECONDS; i++)
     {
-        portTick xTmpTime = xLastTime;
+        portTick xLoopIni = osTaskGetTickCount();
+/**
+ * Simple failure test: inject one failure so task takes more than 1000ms to
+ * execute
+ */
+#if 1
+        // Inject only one failure
+        work = 0;
+        if(i == 2)
+        {
+            work = 1500;
+            usleep(work * 1000); // Task takes 1500ms in execute
+        }
+#endif
+
+/**
+ * Random test: Simulate random loads in the task between 0-1300ms
+ * The task trys to recover from longer loops
+ */
+#if 0
+        work = rand()%1300;
+        usleep(work*1000);
+#endif
+        portTick xLoopEnd = osTaskGetTickCount();
         osTaskDelayUntil(&xLastTime, 1000); //Suspend task for 1 sec
-        LOGD(tag, "Iter %d, tick %u, dt %u us", i, xLastTime, xLastTime-xTmpTime);
-        #if TEST_FAILS
-        if(i == 1)
-            sleep(2);
-        #endif
+        LOGD(tag, "Iter %02d, tick %u, loop %07d us, sleep %07d us, work %07u us", i, xLastTime, xLastTime-xLoopIni, xLastTime-xLoopEnd, work*1000);
     }
 
     portTick xFinalTime = osTaskGetTickCount();
