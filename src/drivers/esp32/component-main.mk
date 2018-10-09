dae174784ca2854704f9d895a88dfdb8dbb57d55
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2018-10-09 11:10:03 -0300

    Fix makefile in ESP32 to add cmdEPS.c

--- src/drivers/esp32/component-main.mk
+++ src/drivers/esp32/component-main.mk
@@ -6,7 +6,7 @@
 # in the build directory. This behaviour is entirely configurable,
 # please read the ESP-IDF documents if you need to do this.
 #
-COMPONENT_OBJS := cmdDRP.o cmdOBC.o globals.o main.o repoData.o taskConsole.o\
+COMPONENT_OBJS := cmdDRP.o cmdOBC.o cmdEPS.o globals.o main.o repoData.o taskConsole.o\
 taskExecuter.o taskHousekeeping.o taskWatchdog.o cmdConsole.o cmdFP.o repoCommand.o\
 taskDispatcher.o taskFlightPlan.o taskInit.o utils.o
 
