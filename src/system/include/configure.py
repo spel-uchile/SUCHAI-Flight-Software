f7def1292e4fdea7dc9d6cb6198ddd11a8747195
Gedoix <diegoortegop@hotmail.com>
2019-01-17 15:45:00 -0300

    Added the database username as a possible argument for the 'configure.py' script, with a default value of the current username. Also, the database username was added as part of the 'config.h' template

--- src/system/include/configure.py
+++ src/system/include/configure.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 from subprocess import Popen, PIPE
+import os
 
 
 def call_git_describe(abbrev=4):
@@ -34,6 +35,7 @@ def parse_args():
     parser.add_argument('--sch_zmq_in', type=str, default="tcp://127.0.0.1:8002")
     parser.add_argument('--sch_st_mode', type=str, default="1")
     parser.add_argument('--sch_st_triple_wr', type=str, default="1")
+    parser.add_argument('--sch_st_pguser', type=str, default=os.environ['USER'])
 
     args = parser.parse_args()
     return args
@@ -64,6 +66,7 @@ def make_config(args, ftemp="config_template.h", fconfig="config.h"):
     config = config.replace("{{SCH_ZMQ_IN}}", args.sch_zmq_in)
     config = config.replace("{{SCH_STORAGE}}", args.sch_st_mode)
     config = config.replace("{{SCH_STORAGE_TRIPLE_WR}}", args.sch_st_triple_wr)
+    config = config.replace("{{SCH_STORAGE_PGUSER}}", args.sch_st_pguser)
 
     with open(fconfig, 'w') as new_config:
         new_config.write(config)
