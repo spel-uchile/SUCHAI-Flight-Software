6267bbdb62af6e900fc991f3998e8a4de294275c
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2019-01-18 15:54:38 -0300

    Fix build scripts

--- src/drivers/nanomind/build.sh
+++ src/drivers/nanomind/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd a3200-sdk-lite-v1.2/
-python2 waf configure build $1 --with-os freertos --enable-if-i2c --with-rtable cidr --disable-param-cmd
+python2 waf configure build $1 --with-os freertos --enable-if-i2c --with-rtable cidr --disable-param-cmd --with-console 4
 cd -
