fe4c80618d96a5b38b0849247769fb686267c813
Gedoix <diegoortegop@hotmail.com>
2019-01-25 19:24:21 -0300

    Fixed installation of avr32-gcc for compiling the nanomind

--- src/drivers/nanomind/install.sh
+++ src/drivers/nanomind/install.sh
@@ -1,26 +1,39 @@
 #!/bin/sh
-# echo "Downloading toolchain..."
-# wget -N data.spel.cl/gs-avr32-toolchain-3.4.2.tar.gz
-# tar -xzf gs-avr32-toolchain-3.4.2.tar.gz
-# cd gs-avr32-toolchain-3.4.2
-# echo "Installing toolchain..."
-# if [ ! -d "~/.local" ]; then
-#     mkdir ~/.local
-# fi
-# if [ ! -d "~/.local/avr32" ]; then
-#     mkdir ~/.local/avr32
-# fi
-# if [ ! -d "~/.local/bin" ]; then
-#     mkdir ~/.local/bin
-# fi
-# if [ ! -d "~/.local/bin" ]; then
-#     mkdir ~/.local/share
-# fi
-# ./install-avr32.sh
-# cd -
+
+echo "Downloading toolchain..."
+wget -N data.spel.cl/gs-avr32-toolchain-3.4.2.tar.gz
+tar -xzf gs-avr32-toolchain-3.4.2.tar.gz
+
+echo "Installing toolchain..."
+cd gs-avr32-toolchain-3.4.2/
+if [[ ! -d "$HOME/.local" ]]; then
+    mkdir ~/.local
+fi
+if [[ ! -d "$HOME/.local/avr32" ]]; then
+    mkdir ~/.local/avr32
+fi
+if [[ ! -d "$HOME/.local/bin" ]]; then
+    mkdir ~/.local/bin
+fi
+if [[ ! -d "$HOME/.local/bin" ]]; then
+    mkdir ~/.local/share
+fi
+./install-avr32.sh
+cd -
+
+echo "Adding directories to PATH..."
+if [[ ":$PATH:" == *":$HOME/.local/bin:$HOME/.local/avr32/bin:"* ]]; then
+  echo "Path was correctly set"
+else
+  echo 'export PATH="$HOME/.local/bin:$HOME/.local/avr32/bin:$PATH"' >> ~/.bashrc
+  source ~/.bashrc
+fi
+
+echo "Checking avr32-gcc Version..."
+avr32-gcc --version
 
 echo "Downloading OBC drivers SDK..."
-if [ ! -d "a3200-sdk-lite-v1.2" ]; then
+if [[ ! -d "a3200-sdk-lite-v1.2" ]]; then
     git clone https://github.com/spel-uchile/suchai-drivers-obc a3200-sdk-lite-v1.2
 else
     cd a3200-sdk-lite-v1.2
@@ -35,7 +48,7 @@ ln -s -f ../../../../src
 cd -
 
 echo "Adding libcsp v1.5.dev to sdk lib folder"
-if [ ! -d "libcsp" ]; then
+if [[ ! -d "libcsp" ]]; then
     git clone https://github.com/libcsp/libcsp
     cd libcsp
     git checkout release-1.5
@@ -57,7 +70,7 @@ ln -s -f ../../libcsp
 cd -
 
 echo "Downloading BUS drivers..."
-if [ ! -d "gs-drivers" ]; then
+if [[ ! -d "gs-drivers" ]]; then
     git clone https://github.com/spel-uchile/suchai-drivers-bus gs-drivers
 else
     cd gs-drivers
