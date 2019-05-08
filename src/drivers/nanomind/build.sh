d8d2b299ad424803165541c8034f856222302ee3
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2019-05-08 15:46:44 -0400

    Fix previous commit errors

--- src/drivers/nanomind/build.sh
+++ src/drivers/nanomind/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd a3200-sdk-lite-v1.2/
-python2 waf configure build $1 --with-os freertos --enable-if-i2c --with-rtable cidr --disable-param-cmd --with-console 4 --clock fm33256b
+python2 waf configure build $1 --with-os freertos --enable-if-i2c --with-rtable cidr --disable-param-cmd --with-console 4
+#python2 waf configure build $1 --with-os freertos --enable-if-i2c --with-rtable cidr --disable-param-cmd
 cd -
