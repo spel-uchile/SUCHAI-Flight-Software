dadce2e6337819152ff17b34516f902eed99470d
Carlos Gonzalez <contacto.carlosgonzalez@gmail.com>
2020-02-28 13:10:10 -0300

    Remove unused install file

--- install.sh
+++
@@ -1,8 +0,0 @@
-#/bin/env bash
-git clone -b simulator git@github.com:spel-uchile/SUCHAI-Flight-Software.git tmp
-mv tmp/* tmp/.[^.]* .
-rm -r ./tmp
-build_cmd="LINUX SIMULATOR --name SIMULATOR --id 1 --hk 1 --com 1 --fp 0 --sen 0 --con 0 --node 1 --st_mode 0 --drivers"
-echo "Configure (and build) SUCHAI-FS using:"
-echo $build_cmd
-python3 compile.py $build_cmd
