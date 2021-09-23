3ad0101f93e4fce4f1582c84cef3fe7d23493f04
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2021-09-23 00:31:05 -0300

    Fix toolchain installation scripts

--- src/drivers/nanomind/install-toolchain.sh
+++ src/drivers/nanomind/install-toolchain.sh
@@ -8,5 +8,6 @@ else
 fi
 
 cd suchai-docker/suchai-fs/avr32-toolchain-3.4.2
-source /install-avr32.sh 3.4.3.820 6.1.3.1475 $HOME/.local
+chmod +x install-avr32.sh
+./install-avr32.sh 3.4.3.820 6.1.3.1475 $HOME/.local
 cd -
