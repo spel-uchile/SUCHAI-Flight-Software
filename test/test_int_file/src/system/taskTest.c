7a95cbf53f6bc7c3b8f083c27dd7dc25b6413b6c
Riul1999 <rodrigo.urrea@ing.uchile.cl>
2021-11-25 11:40:25 -0300

    Second stage of test_int_file, missing smaller files, check why it takes to long and when all data was sent

--- test/test_int_file/src/system/taskTest.c
+++ test/test_int_file/src/system/taskTest.c
@@ -10,49 +10,87 @@ void taskTest(void *params){
 
     int node = 3;
 
+    char fname[64] = "/text.txt";
     char filename[192];
-    snprintf(filename, 192, "%s/text.txt", current_dir);
+    strcat(filename, current_dir);
+    strcat(filename, fname);
 
     char buf[256];
     snprintf(buf, 256, "tm_send_file %s %d", filename, node);
     cmd_t *cmd = cmd_build_from_str(buf);
     cmd_send(cmd);
 
+    /*
+     * TODO use small file and see why it takes to long to send the data
+    char image[64] = "/image.jpeg";
     char imagename[192];
-    snprintf(imagename, 192, "%s/image.jpeg", current_dir);
+    strcat(imagename, current_dir);
+    strcat(imagename, image);
 
     char buf2[256];
-    snprintf(buf2, 256, "tm_send_file %s %d", filename, node);
-    cmd = cmd_build_from_str(buf);
+    snprintf(buf2, 256, "tm_send_file %s %d", imagename, node);
+    cmd = cmd_build_from_str(buf2);
     cmd_send(cmd);
 
+    char fname2[64] = "/alice.txt";
     char filename_part[192];
-    snprintf(filename_part, 192, "%s/alice29.txt", current_dir);
+    strcat(filename_part, current_dir);
+    strcat(filename_part, fname2);
 
-    // TODO know how to use tm_send_file_part command
-    //char buf3[256];
-    //snprintf(buf3, 256, "tm_send_file_part %s %d", filename_part, node);
-    //cmd = cmd_build_from_str(buf3);
-    //cmd_send(cmd);
 
+    char buf3[256];
+    int file_id = 1;
+    int start_frame = 0;
+    int end_frame = -1;
+    snprintf(buf3, 256, "tm_send_file_part %s %d %d %d %d", filename_part, file_id, start_frame, end_frame, node);
+    cmd = cmd_build_from_str(buf3);
+    cmd_send(cmd);
+    */
+
+    // Here need a condition to check all data was sent
     // Wait until all data was sent
-    sleep(5);
+    sleep(10);
 
-    // TODO know where are and how retrieve the data sent
-    //received_text = current_dir + "bla_bla.txt";
-    //char buf_cmp1[256];
-    //snprintf(buf_cmp1, 256, "cmp_file %s %s", filename, received_text);
-    //cmd = cmd_build_from_str(buf_cmp1);
-    //cmd_send(cmd);
+    char received_text[192];
+    strcat(received_text, current_dir);
+    strcat(received_text, "/recv_files");
+    strcat(received_text, fname);
 
-    //received_image = current_dir + "bla_bla.txt";
-    //char buf_cmp2[256];
-    //snprintf(buf_cmp1, 256, "cmp_file %s %s", imagename, received_text);
-    //cmd = cmd_build_from_str(buf_cmp2);
-    //cmd_send(cmd);
+    char buf_cmp1[256];
+    snprintf(buf_cmp1, 256, "file_cmp %s %s", filename, received_text);
+    LOGI(tag, buf_cmp1);
+    cmd = cmd_build_from_str(buf_cmp1);
+    cmd_send(cmd);
 
-    //TODO receive and merge data
+    /*
+     * TODO how to check the large file was totally merged
+    char received_image[192];
+    strcat(received_image, current_dir);
+    strcat(received_image, "/recv_files");
+    strcat(received_image, image);
 
+    char buf_cmp2[256];
+    snprintf(buf_cmp2, 256, "file_cmp %s %s", imagename, received_image);
+    cmd = cmd_build_from_str(buf_cmp2);
+    cmd_send(cmd);
+
+    char received_text2[192];
+    strcat(received_text2, current_dir);
+    strcat(received_text2, "/recv_files");
+    strcat(received_text2, fname2);
+
+    char buf4[256];
+    snprintf(buf4, 256, "tm_merge_file %s %d", fname2, file_id);
+    cmd = cmd_build_from_str(buf4);
+    cmd_send(cmd);
+
+    char buf_cmp3[256];
+    snprintf(buf_cmp3, 256, "file_cmp %s %s", filename_part, received_text2);
+    cmd = cmd_build_from_str(buf_cmp3);
+    cmd_send(cmd);
+    */
+
+    // TODO delete all files created
 
     osTaskDelete(NULL);
 }
